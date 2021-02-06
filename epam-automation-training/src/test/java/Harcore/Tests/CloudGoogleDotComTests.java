89f54cfe4559df5792e95cb2f47f33a0d4c71ab6
Vladimir <ryzhenkov_v@list.ru>
2021-02-06 14:09:34 +0500

    Selenium WebDriver Added Hardcore Task

---
+++ epam-automation-training/src/test/java/Harcore/Tests/CloudGoogleDotComTests.java
@@ -0,0 +1,42 @@
+package Harcore.Tests;
+
+import Harcore.PageObject.CloudGoogleDotComPageObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class CloudGoogleDotComTests {
+    private WebDriver driver;
+    private CloudGoogleDotComPageObject webPage;
+    @BeforeMethod(alwaysRun = true)
+    public  void resetBrowser() {
+        driver=new ChromeDriver();
+        webPage=new CloudGoogleDotComPageObject(driver)
+                .openPage()
+                .submitSearchRequest()
+                .openRequestedSearchResult()
+                .fillEstimateFormFields()
+                .submitAddToEstimate()
+                .clickEmailEstimate()
+                .generateTemporaryEmail()
+                .fillEmailEstimateForm()
+                .clickSendEmail();
+    }
+
+    @Test(description = "Test for https://cloud.google.com/")
+    public  void checkMonthlyPayment() {
+        Assert.assertEquals(webPage.getTotalEstimatedCostFromEmail(), 1082, 1.0
+                , "Monthly payment value differ from expected!");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public  void quiteBrowser() {
+        driver.quit();
+        driver=null;
+    }
+}
